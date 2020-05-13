3a783525aa545ee447aa0ca4c9a8ad712283eebd
Olexeus <fedorenkoalex23@gmail.com>
2020-05-13 17:00:00 -0500

    Oleksii commit

--- Prove4/src/MainActivity.java
+++ Prove4/src/MainActivity.java
@@ -1,5 +1,7 @@
 public class MainActivity {
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is Ben");
+        System.out.println("What's up Ben. This is Oleksii");
     }
 }
